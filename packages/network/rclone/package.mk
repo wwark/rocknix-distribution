9632735ffd89c184cc85aba89a2180bce474d9ec
Max Engel <max.engel@me.com>
2025-03-25 20:28:31 -0400

    new code structure and functions to make upgrades go more smoothly when there are function changes.
    various improvements after running tests
    
    additional improvements after testing
    
    bug fixes and logging improvements
    
    bug fixes
    
    bumped rclone version
    
    additional fixes
    
    bug fixes
    
    additional fixes
    
    cleaned up terminal output
    
    additional fixes and optimizations
    
    minor usability tweaks
    
    improved comments

--- packages/network/rclone/package.mk
+++ packages/network/rclone/package.mk
@@ -3,7 +3,7 @@
 # Copyright (C) 2023 JELOS (https://github.com/JustEnoughLinuxOS)
 
 PKG_NAME="rclone"
-PKG_VERSION="1.69.0"
+PKG_VERSION="1.69.1"
 PKG_DEPENDS_TARGET="toolchain fuse rsync"
 PKG_LONGDESC="rsync for cloud storage"
 PKG_TOOLCHAIN="manual"
@@ -21,9 +21,13 @@ PKG_URL="https://downloads.rclone.org/v${PKG_VERSION}/rclone-v${PKG_VERSION}-lin
 PKG_RCLONE="rclone-v${PKG_VERSION}-linux-${RCLONE_ARCH}/rclone"
 
 unpack() {
+  # Create build directory
   mkdir -p ${PKG_BUILD}
+  # Rename the downloaded zip to include architecture for better tracking
   mv ${SOURCES}/rclone/rclone-${PKG_VERSION}.zip ${SOURCES}/rclone/rclone-${PKG_VERSION}-${RCLONE_ARCH}.zip
+  # Extract the binary package to the build directory
   unzip ${SOURCES}/rclone/rclone-${PKG_VERSION}-${RCLONE_ARCH}.zip -d ${PKG_BUILD}/
+  # Remove downloaded zip files to conserve space
   rm -f ${SOURCES}/rclone/rclone-${PKG_VERSION}*
 }
 
@@ -33,6 +37,7 @@ makeinstall_target() {
   cp rclonectl ${INSTALL}/usr/bin/
   cp cloud_backup ${INSTALL}/usr/bin/
   cp cloud_restore ${INSTALL}/usr/bin/
+  cp cloud_sync_helper ${INSTALL}/usr/bin/
   cp ${PKG_BUILD}/${PKG_RCLONE} ${INSTALL}/usr/bin/
   chmod 0755 ${INSTALL}/usr/bin/*
   cp rsync-rules.conf ${INSTALL}/usr/config/
@@ -40,6 +45,7 @@ makeinstall_target() {
   cp cloud_sync-rules.txt ${INSTALL}/usr/config/
   cp cloud_sync.conf ${INSTALL}/usr/config/
   cp cloud_sync.conf.defaults ${INSTALL}/usr/config/
+  cp cloud_sync-rules.txt.defaults ${INSTALL}/usr/config/
   chmod 755 ${INSTALL}/usr/bin/rclone
   mkdir -p ${INSTALL}/usr/config/modules
   ln -sf /usr/bin/cloud_backup ${INSTALL}/usr/config/modules/cloud_backup.sh
